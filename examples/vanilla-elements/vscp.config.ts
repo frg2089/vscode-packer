af8d265f84e517213621c541fe31b7c6e89ab46d
舰队的偶像-岛风酱! <frg2089@outlook.com>
2025-01-05 11:29:02 +0800

    update
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

--- examples/vanilla-elements/vscp.config.ts
+++ examples/vanilla-elements/vscp.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vscode-packer'
-import sass from 'vscode-packer/sass'
-import typescript from 'vscode-packer/typescript'
+import { defineConfig } from '@frg2089/vscode-packer'
+import sass from '@frg2089/vscode-packer/sass'
+import typescript from '@frg2089/vscode-packer/typescript'
 
 export default defineConfig({
   plugins: [typescript(), sass()],
